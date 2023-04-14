fbbd7ffe4b2146dccd7319c7aead1779d7bd4311
kim <kimberlypangilinan2001@gmail.com>
2023-04-14 08:13:32 +0800

    initializes flatlist realtime update in firebase

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,31 +1,65 @@
-import React, { useState,useEffect } from 'react'
-import { ScrollView,TextInput,Text, View ,Pressable,StyleSheet} from 'react-native'
-import {Header} from '../components/Header'
-import { collection, addDoc } from "firebase/firestore"; 
-import {db} from '../config/firebase'
-import { auth,  } from "../config/firebase";
+import React, { useState, useEffect } from 'react';
+import { ScrollView, FlatList, TextInput, Text, View, Pressable, StyleSheet } from 'react-native';
+import { Header } from '../components/Header';
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { db } from '../config/firebase';
+import { auth } from "../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import {  onSnapshot, query, where, } from "firebase/firestore";
+
+
+const Item = ({name,id}) => (
+  <View style={styles.innerContainer}>
+    <Text style={styles.itemText}>{name}</Text>
+    <Text>{id}</Text>
+  </View>
+)
+const Seperator =()=> <View style={{borderBottomWidth:0.5,borderColor:'#3c343d'}}/>
+const Header1 =()=> <View  style={styles.MenuContainer}><Text style={styles.itemMenu}>Menu</Text></View>
+
+
 const HomeScreen = () => {
-  const [isLoggedIn,setIsLoggedIn] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState('');
+  const [cities, setCities] = useState([]);
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       if (user) {
         setIsLoggedIn(false);
-      }else{
+      } else {
         setIsLoggedIn(true);
       }
-    })
-
-    return unsubscribe
-  }, [])
-  // Add a new document with a generated id.
-  const addEvent =async()=>{
-    const docRef = await addDoc(collection(db, "cities"), {
-  name: "Tokyo",
-  country: auth.currentUser?.email
+    });
+    const q = query(collection(db, "cities"));
+    const unsubscribeCities = onSnapshot(q, (querySnapshot) => {
+      const cities = [];
+      querySnapshot.forEach((doc) => {
+          cities.push({id: doc.id, ...doc.data()});
+      });
+  setCities(cities);
 });
-console.log("Document written with ID: ", docRef.id);
-  }
+   
+  //  const unsubscribeCities = onSnapshot(collection(db, "cities"), (querySnapshot) => {
+  //    const cityData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+ //     setCities(cityData);
+ //   });
+  
+  return () => {
+    unsubscribeCities();
+    unsubscribe();
+  };
+}, []);
 
+  // Add a new document with a generated id.
+  const addEvent = async () => {
+    const docRef = await addDoc(collection(db, "cities"), {
+      name: "Tokyo",
+      country: auth.currentUser?.email
+    });
+    console.log("Document written with ID: ", docRef.id);
+  };
+
+  const renderItem = ({item}) => <Item name = {item.name} id={item.id}/>
 
   return (
     <View style={styles.container}>
@@ -35,6 +69,13 @@ console.log("Document written with ID: ", docRef.id);
         <Text>Email: {auth.currentUser?.email}</Text>
         <Pressable onPress={addEvent}><Text>Add</Text></Pressable>
 
+        <FlatList 
+        data={cities} 
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={Seperator}
+        ListHeaderComponent={Header1}
+      />
       </View>
     </View>
   );
@@ -51,4 +92,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
+
 export default HomeScreen;
